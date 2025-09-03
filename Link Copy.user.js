f6b243f9df8bb993b26cf0f5eaa05fd569dd7a55
DandyClubs <dandyclubs@gmail.com>
2025-09-03 18:28:28 +0900

    el.href =

--- Link Copy.user.js
+++ Link Copy.user.js
@@ -3174,7 +3174,7 @@ function listToDo(areas, type = 'Default') {
 
     // 2) Filter and normalize each link
     for (const el of seenAnchors) {
-        el.href.replace(/\?site.+/, '');
+        el.href = el.href.replace(/\?site.+/, '');
         // Skip filtering patterns
         if (checkSkipFilter(el)) continue;
         // Skip links with image children for certain hosts
