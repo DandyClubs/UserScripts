9213c2595c60db390a6416e5ae1c86474c398c8c
DandyClubs <dandyclubs@gmail.com>
2026-02-15 19:54:00 +0900

    Update Copy Links Title.user.js

--- Copy Links Title.user.js
+++ Copy Links Title.user.js
@@ -713,8 +713,10 @@ function extractInfoFromText(infoLines, fallbackTitle, options = {}) {
 
     const FeaturingLine = infoLines.find(line => line.match(/(Featuring|Title)/i));
     const Featuring = FeaturingLine ? FeaturingLine.replace(/(Featuring|Title)\s?:/i, '').trim() : '';
+    const cleanInfoLines = infoLines.map(line => line.replace(/^Video\s?info:$/i, '').trim())).filter(Boolean);
+    console.log({ cleanInfoLines });
 
-    infoLines.some((line, index) => {
+    cleanInfoLines.some((line, index) => {
         // 1. ID 추출 (최적화)
         if (!ID) {
             const idMatch = line.match(SearchID);
@@ -741,7 +743,7 @@ function extractInfoFromText(infoLines, fallbackTitle, options = {}) {
             // 특정 패턴(Title:)으로 시작하는 경우 예외 처리
             if (!Title && /^Title\s?:/i.test(line)) {
                 Title = line.replace(/^Title\s?:/i, '').trim();
-            }
+            }            
 
             if (Title) Title = Title.trim() + ' ';
         }
