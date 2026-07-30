99ffc5d61f6f235978f0e6fcde63646da6a426d2
DandyClubs <dandyclubs@gmail.com>
2026-07-30 16:06:22 +0900

    Update Copy Links Title.user.js

--- Copy Links Title.user.js
+++ Copy Links Title.user.js
@@ -810,7 +810,7 @@ function extractInfoFromText(infoLines, fallbackTitle, options = {}) {
         Title = `${Featuring} - ${Title}`;
     }
 
-    const replaceEx = /(Actress\sand\sTitle\sVideo|Details\s\/\sInformations|Thumbnails\s\/\sScreenshots|General\s\/\sNames|Asianmania_|New!.+[\d+]|Re|^File\sName|Title|^File)(\s?([::：-]*))?/i;
+    const replaceEx = /(Actress\sand\sTitle\sVideo|Details\s\/\sInformations|Thumbnails\s\/\sScreenshots|General\s\/\sNames|Asianmania_|New!.+[\d+]|^Re|^File\sName|Title|^File)(\s?([::：-]*))?/i;
     console.log({ Title });
     Title = Title ? Title.replace(replaceEx, '').trim() : '';
     console.log({ Title });
