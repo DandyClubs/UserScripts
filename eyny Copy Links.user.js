da0dc4b44e820bc70567694897e278c1ff0ab604
DandyClubs <dandyclubs@gmail.com>
2026-09-01 17:15:29 +0900

    Update eyny Copy Links.user.js

--- eyny Copy Links.user.js
+++ eyny Copy Links.user.js
@@ -1181,28 +1181,18 @@ async function KatCopyItems() {
 function JDownloader(JdownloaderData, PackageName, PW, Source) {
     if (JdownloaderData) {
         let data = new URLSearchParams();
-        if (PW) {
-            data.append(`passwords`, PW);
-        }
+        data.append(`urls`, JdownloaderData);        
         if (Source) {
             data.append(`source`, Source);
-        }
-        data.append(`urls`, JdownloaderData);
+        }        
         //data.append(`referer`, PAGE_URL);
         if (PackageName) {
             data.append(`package`, PackageName);
         }
-        /*
-        fetch('http://127.0.0.1:9666/flash/add', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                'Access-Control-Allow-Origin': 'http://localhost:9666',
-            },
-            body: data
-        });
-        */
-
+        if (PW) {
+            data.append(`passwords`, PW + "\n");            
+        }
+        
         GM_xmlhttpRequest({
             method: "POST",
             url: "http://localhost:9666/flash/add",
